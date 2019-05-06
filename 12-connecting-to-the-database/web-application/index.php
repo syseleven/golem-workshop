06848fb634f2276c4a2ba1dff0be1e9a5324ce39
Bastian Hofmann <bashofmann@gmail.com>
2019-05-06 20:42:59 +0200

    fix db connection

--- 12-connecting-to-the-database/web-application/index.php
+++ 12-connecting-to-the-database/web-application/index.php
@@ -11,7 +11,7 @@ $app->get('/', function (Request $request, Response $response) {
     $response->getBody()->write('<h1>Hello at ' . $_ENV['COMPANY_NAME'] . '</h1>');
     $response->getBody()->write('<h3>The current passcode is: ' . file_get_contents('/etc/web-application/passcode.txt') . '</h3>');
 
-    $mysqli = new mysqli($_ENV['DB_HOST'], $_ENV['DB_USER'], $_ENV['DB_PASSWORD'], "mysql-svc");
+    $mysqli = new mysqli($_ENV['DB_HOST'], $_ENV['DB_USER'], $_ENV['DB_PASSWORD'], "mysql");
 
     if ($result = $mysqli->query("SELECT * from db")) {
         $response->getBody()->write("Select returned " . $result->num_rows . " rows.");
