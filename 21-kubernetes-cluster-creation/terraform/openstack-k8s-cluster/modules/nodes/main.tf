12ca062a46a372b4a24450bfd7558443f9ce173f
Simon <s.pearce@syseleven.de>
2019-10-08 22:31:13 +0200

    updated openstack terraform

--- 21-kubernetes-cluster-creation/terraform/openstack-k8s-cluster/modules/nodes/main.tf
+++ 21-kubernetes-cluster-creation/terraform/openstack-k8s-cluster/modules/nodes/main.tf
@@ -45,17 +45,3 @@
     floating_ip = "${element(openstack_compute_floatingip_v2.floating_ip.*.address, count.index)}"
     instance_id = "${element(openstack_compute_instance_v2.instance.*.id, count.index)}"
   }
-
-# Module outputs
-
-output "local_ip_v4" {
-  value = ["${openstack_compute_instance_v2.instance.*.network.0.fixed_ip_v4}"]
-}
-
-output "public_ip" {
-  value = ["${openstack_compute_floatingip_v2.floating_ip.*.address}"]
-}
-
-output "hostnames" {
-  value = ["${openstack_compute_instance_v2.instance.*.name}"]
-}
