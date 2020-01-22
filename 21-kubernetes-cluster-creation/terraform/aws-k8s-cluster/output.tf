4ecd6ca513fd708c984b1e1db7e87d007f944e6a
Simon <s.pearce@syseleven.de>
2020-01-22 13:53:04 +0100

    updated aws terraform 0.12

--- 21-kubernetes-cluster-creation/terraform/aws-k8s-cluster/output.tf
+++ 21-kubernetes-cluster-creation/terraform/aws-k8s-cluster/output.tf
@@ -1,5 +1,6 @@
 # Elastic Load Balancer output
 
 output "master_address" {
-  value = "${aws_elb.master.dns_name}"
+  value = aws_elb.master.dns_name
 }
+
--- 21-kubernetes-cluster-creation/terraform/aws-k8s-cluster/variables.tf
+++ 21-kubernetes-cluster-creation/terraform/aws-k8s-cluster/variables.tf
@@ -1,9 +1,10 @@
 variable "public_key_path" {
   description = "Enter the path to the SSH Public Key to add to AWS."
-  default = "~/.ssh/id_rsa.pub"
+  default     = "~/.ssh/id_rsa.pub"
 }
 
 variable "aws_region" {
   description = "AWS region to launch servers."
   default     = "eu-central-1"
 }
+
